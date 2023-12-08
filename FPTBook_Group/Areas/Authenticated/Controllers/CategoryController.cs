7fabe06c866355a76236cd948e9ad104e18fdd1b
ltn23 <nhanlt232@gmail.com>
2023-12-08 14:33:33 +0700

    add CategoryController

---
+++ FPTBook_Group/Areas/Authenticated/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FPTBook_Group.Areas.Authenticated.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
