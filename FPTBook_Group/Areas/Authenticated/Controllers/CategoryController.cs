edd8f3fc41984a4963f02f239e18fe4f438bae78
ltn23 <nhanlt232@gmail.com>
2023-12-08 14:39:09 +0700

    update CategoryController

--- FPTBook_Group/Areas/Authenticated/Controllers/CategoryController.cs
+++ FPTBook_Group/Areas/Authenticated/Controllers/CategoryController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FPTBook_Group.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FPTBook_Group.Areas.Authenticated.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
