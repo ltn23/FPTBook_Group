8c031e7bf6da47304ae8d8e1fec7289e3884eba3
Phan Đình Thắng <106921106+thangpdgcd@users.noreply.github.com>
2023-12-09 17:40:39 +0700

    mvc-publishcompany add-migration and conttroller

---
+++ FPTBook_Group/ModelsCRUD/PublishCompany/AddCompanyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FPTBook_Group.ModelsCRUD.PublishCompany
+{
+	public class AddCompanyViewModel
+	{
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public string Adress { get; set; }
+	}
+}
---
+++ FPTBook_Group/ModelsCRUD/PublishCompany/UpdateCompanyView.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FPTBook_Group.ModelsCRUD.PublishCompany
+{
+	public class UpdateCompanyView
+	{
+		[Key]
+		public int PublishingCompanyId { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public string Adress { get; set; }
+	}
+}
